de97d6136251d4fb148db9ea5ab3891a015b6d04
GoldGain <martinmakau2005@gmail.com>
2026-06-30 18:29:28 +0300

    feat: add ReportCards placeholder page

---
+++ src/pages/dashboard/school-admin/ReportCards.tsx
@@ -0,0 +1,5 @@
+import PlaceholderPage from '@/components/PlaceholderPage';
+
+export default function ReportCards() {
+  return <PlaceholderPage title="Report Cards" description="Generate and print student report cards. This feature is coming soon." />;
+}
